8deb5ed72d9ca8a844380d7c1ec392f824193e43
changduchen <changduchen@163.com>
2017-08-11 17:04:20 +0800

    modified blog site with markdown

--- Auth_System/urls.py
+++ Auth_System/urls.py
@@ -4,10 +4,21 @@ from django.views.generic.base import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='users/index.html'), name='index'),
     url(r'^users/', include('users.urls', namespace='users')),
     url(r'^upload/', include('upload_app.urls', namespace='upload')),
     url(r'^blog/', include('blog.urls', namespace='blog')),
+
+    url(r'sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
