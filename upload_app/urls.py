8fc8cea2cf79a399d23f6a70bcebd39d5eff3175
changduchen <changduchen@163.com>
2017-09-06 18:43:32 +0800

    change class based views

--- upload_app/urls.py
+++ upload_app/urls.py
@@ -7,15 +7,23 @@ from . import views
 urlpatterns = [
     url(r'^$', views.upload),
     url(r'^upload_file', views.upload_file, name='upload_file'),
-    url(r'^upload_image/(?P<album_id>[0-9]+)/$', views.upload_image, name='upload_image'),
-    url(r'^show_image/(?P<album_id>[0-9]+)/$', views.show_image, name='show_image'),
-    url(r'^show_image/all/$', views.show_image_all, name='show_image_all'),
+
+    # url(r'^upload_image/(?P<album_id>[0-9]+)/$', views.upload_image, name='upload_image'),
+    url(r'^upload_image/(?P<album_id>[0-9]+)/$', views.UploadImageView.as_view(), name='upload_image'),
+
+    # url(r'^show_image/(?P<album_id>[0-9]+)/$', views.show_image, name='show_image'),
+    url(r'^show_image/(?P<album_id>[0-9]+)/$', views.ShowImageView.as_view(), name='show_image'),
+
+    # url(r'^show_image/all/$', views.show_image_all, name='show_image_all'),
+    url(r'^show_image/all/$', views.ShowImageAllView.as_view(), name='show_image_all'),
 
     url(r'^show_image/all/name$', views.show_image_all_by_name, name='show_image_all_by_name'),
     url(r'^show_image/all/time$', views.show_image_all_by_time, name='show_image_all_by_time'),
     url(r'^show_image/all/size$', views.show_image_all_by_size, name='show_image_all_by_size'),
 
-    url(r'^show_file/$', views.show_file, name='show_file'),
+    # url(r'^show_file/$', views.show_file, name='show_file'),
+    url(r'^show_file/$', views.ShowFileView.as_view(), name='show_file'),
+
     url(r'^show_file_list/$', views.show_file_list, name='show_file_list'),
     url(r'^show_file_detail/(?P<file_id>[0-9]+)/$', views.show_file_detail, name='show_file_detail'),
 
@@ -24,9 +32,17 @@ urlpatterns = [
     url(r'^image_restore/(?P<image_id>[0-9]+)/$', views.image_restore, name='image_restore'),
     url(r'^trash_detail/$', views.trash_detail, name='trash_detail'),
     url(r'^file_delete/(?P<file_id>[0-9]+)/$', views.file_delete, name='file_delete'),
-    url(r'^add_album/$', views.add_album, name='add_album'),
-    url(r'^show_album/$', views.show_album, name='show_album'),
-    url(r'^delete_album/(?P<album_id>[0-9]+)/$', views.delete_album, name='delete_album'),
-    url(r'^edit_album/(?P<album_id>[0-9]+)/$', views.edit_album, name='edit_album'),
+
+    # url(r'^add_album/$', views.add_album, name='add_album'),
+    url(r'^add_album/$', views.AddAlbumView.as_view(), name='add_album'),
+
+    # url(r'^show_album/$', views.show_album, name='show_album'),
+    url(r'^show_album/$', views.ShowAlbumView.as_view(), name='show_album'),
+
+    # url(r'^delete_album/(?P<album_id>[0-9]+)/$', views.delete_album, name='delete_album'),
+    url(r'^delete_album/(?P<album_id>[0-9]+)/$', views.DeleteAlbumView.as_view(), name='delete_album'),
+
+    # url(r'^edit_album/(?P<album_id>[0-9]+)/$', views.edit_album, name='edit_album'),
+    url(r'^edit_album/(?P<album_id>[0-9]+)/$', views.EditAlbumView.as_view(), name='edit_album'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- upload_app/views.py
+++ upload_app/views.py
@@ -10,66 +10,151 @@ from .forms import FileUploadForm, ImageUploadForm, AlbumCreateForm
 from .models import FileModel, ImageModel, AlbumModel
 from .tasks import transcode_video_task, generate_thumbnail_task
 
+from django.views.generic import TemplateView, FormView, View
+from django.core.urlresolvers import reverse_lazy
+
 
 @login_required
 def upload(request):
     return render(request, 'upload_app/upload.html')
 
 
-@login_required
-def add_album(request):
-    user = request.user
-    if request.method == 'POST':
+# @login_required
+# def add_album(request):
+#     user = request.user
+#     if request.method == 'POST':
+#         form = AlbumCreateForm(request.POST)
+#         if form.is_valid():
+#             form = form.save(commit=False)
+#             form.user = user
+#             form.pub_date = timezone.now()
+#             form.save()
+#             return HttpResponseRedirect('/upload/show_album/')
+#     else:
+#         form = AlbumCreateForm()
+#     return render(request, 'upload_app/add_album.html', {'form': form})
+
+
+class AddAlbumView(FormView):
+    form_class = AlbumCreateForm
+    success_url = reverse_lazy('upload:show_album')
+    template_name = 'upload_app/add_album.html'
+
+    def post(self, request, *args, **kwargs):
         form = AlbumCreateForm(request.POST)
         if form.is_valid():
             form = form.save(commit=False)
-            form.user = user
+            form.user = request.user
             form.pub_date = timezone.now()
             form.save()
             return HttpResponseRedirect('/upload/show_album/')
-    else:
-        form = AlbumCreateForm()
-    return render(request, 'upload_app/add_album.html', {'form': form})
 
 
-@login_required
-def show_album(request):
-    user = request.user
-    albums = AlbumModel.objects.filter(user=user)
-    return render(request, 'upload_app/show_album.html', {'albums': albums})
+# @login_required
+# def show_album(request):
+#     user = request.user
+#     albums = AlbumModel.objects.filter(user=user)
+#     return render(request, 'upload_app/show_album.html', {'albums': albums})
 
 
-@login_required
-def edit_album(request, album_id):
-    user = request.user
-    album = AlbumModel.objects.get(pk=album_id)
-    if request.method == 'POST':
-        form = AlbumCreateForm(request.POST, instance=album)
+class ShowAlbumView(TemplateView):
+    template_name = 'upload_app/show_album.html'
+
+    def get_context_data(self, **kwargs):
+        user = self.request.user
+        albums = AlbumModel.objects.filter(user=user)
+        context = super(ShowAlbumView, self).get_context_data(**kwargs)
+        context['albums'] = albums
+        return context
+
+
+# @login_required
+# def edit_album(request, album_id):
+#     user = request.user
+#     album = AlbumModel.objects.get(pk=album_id)
+#     if request.method == 'POST':
+#         form = AlbumCreateForm(request.POST, instance=album)
+#         if form.is_valid():
+#             form = form.save(commit=False)
+#             form.user = user
+#             form.pub_date = timezone.now()
+#             form.save()
+#             return HttpResponseRedirect('/upload/show_image/' + album_id)
+#     else:
+#         form = AlbumCreateForm(instance=album)
+#     return render(request, 'upload_app/edit_album.html', {'form': form})
+
+
+class EditAlbumView(View):
+    form_class = AlbumCreateForm
+    template_name = 'upload_app/edit_album.html'
+
+    def get(self, request, *args, **kwargs):
+        album = AlbumModel.objects.get(pk=kwargs.get("album_id"))
+        form = self.form_class(instance=album)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        album = AlbumModel.objects.get(pk=kwargs.get("album_id"))
+        form = self.form_class(request.POST, instance=album)
         if form.is_valid():
             form = form.save(commit=False)
-            form.user = user
+            form.user = request.user
             form.pub_date = timezone.now()
             form.save()
-            return HttpResponseRedirect('/upload/show_image/' + album_id)
-    else:
-        form = AlbumCreateForm(instance=album)
-    return render(request, 'upload_app/edit_album.html', {'form': form})
+            return HttpResponseRedirect('/upload/show_image/' + kwargs.get("album_id"))
+        return render(request, self.template_name, {'form': form})
 
 
-@login_required
-def delete_album(request, album_id):
-    album = AlbumModel.objects.get(pk=album_id)
-    album.delete()
-    return HttpResponseRedirect('/upload/show_album/')
+# @login_required
+# def delete_album(request, album_id):
+#     album = AlbumModel.objects.get(pk=album_id)
+#     album.delete()
+#     return HttpResponseRedirect('/upload/show_album/')
 
 
-@login_required
-def upload_image(request, album_id):
-    user = request.user
-    username = user.username
-    album = AlbumModel.objects.get(pk=album_id)
-    if request.method == 'POST':
-        form = ImageUploadForm(request.POST, request.FILES)
+class DeleteAlbumView(View):
+
+    def get(self, request, *args, **kwargs):
+        AlbumModel.objects.get(pk=kwargs.get("album_id")).delete()
+        return HttpResponseRedirect('/upload/show_album/')
+
+
+# @login_required
+# def upload_image(request, album_id):
+#     user = request.user
+#     username = user.username
+#     album = AlbumModel.objects.get(pk=album_id)
+#     if request.method == 'POST':
+#         form = ImageUploadForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form = form.save(commit=False)
+#             form.user = user
+#             form.album = album
+#             form.size = request.FILES['image'].size
+#             form.name = request.FILES['image'].name
+#             form.pub_date = timezone.now()
+#             form.save()
+#             return HttpResponseRedirect('/upload/show_image/' + album_id)
+#     else:
+#         form = ImageUploadForm()
+#     return render(request, 'upload_app/upload_image.html', {'form': form, 'username': username})
+
+
+class UploadImageView(View):
+    form_class = ImageUploadForm
+    template_name = 'upload_app/upload_image.html'
+
+    def get(self, request, *args, **kwargs):
+        album = AlbumModel.objects.get(pk=kwargs.get("album_id"))
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        username = user.username
+        album = AlbumModel.objects.get(pk=kwargs.get("album_id"))
+        form = self.form_class(request.POST, request.FILES)
         if form.is_valid():
             form = form.save(commit=False)
             form.user = user
@@ -78,31 +163,59 @@ def upload_image(request, album_id):
             form.name = request.FILES['image'].name
             form.pub_date = timezone.now()
             form.save()
-            return HttpResponseRedirect('/upload/show_image/' + album_id)
-    else:
-        form = ImageUploadForm()
-    return render(request, 'upload_app/upload_image.html', {'form': form, 'username': username})
+            return HttpResponseRedirect('/upload/show_image/' + kwargs.get("album_id"))
+        return render(request, self.template_name, {'form': form, 'username': username})
 
 
-@login_required
-def show_image(request, album_id):
-    user = request.user
-    username = user.username
-    album = AlbumModel.objects.get(pk=album_id)
-    images = ImageModel.objects.filter(user=user, album=album, status=1)
-    content = {'images': images, 'username': username, 'album': album}
-    return render(request, 'upload_app/show_image.html', content)
+# @login_required
+# def show_image(request, album_id):
+#     user = request.user
+#     username = user.username
+#     album = AlbumModel.objects.get(pk=album_id)
+#     images = ImageModel.objects.filter(user=user, album=album, status=1)
+#     content = {'images': images, 'username': username, 'album': album}
+#     return render(request, 'upload_app/show_image.html', content)
 
 
-@login_required
-def show_image_all(request):
-    user = request.user
-    username = user.username
-    images = ImageModel.objects.filter(user=user, status=1)
-    files = FileModel.objects.filter(user=user)
-    return render(request, 'upload_app/show_image_all.html', {'username': username,
-                                                              'images': images,
-                                                              'files': files, })
+class ShowImageView(TemplateView):
+    template_name = 'upload_app/show_image.html'
+
+    def get_context_data(self, **kwargs):
+        user = self.request.user
+        username = user.username
+        album = AlbumModel.objects.get(pk=kwargs.get("album_id"))
+        images = ImageModel.objects.filter(user=user, album=album, status=1)
+        context = super(ShowImageView, self).get_context_data(**kwargs)
+        context['images'] = images
+        context['username'] = username
+        context['album'] = album
+        return context
+
+
+# @login_required
+# def show_image_all(request):
+#     user = request.user
+#     username = user.username
+#     images = ImageModel.objects.filter(user=user, status=1)
+#     files = FileModel.objects.filter(user=user)
+#     return render(request, 'upload_app/show_image_all.html', {'username': username,
+#                                                               'images': images,
+#                                                               'files': files, })
+
+
+class ShowImageAllView(TemplateView):
+    template_name = 'upload_app/show_image_all.html'
+
+    def get_context_data(self, **kwargs):
+        user = self.request.user
+        username = user.username
+        images = ImageModel.objects.filter(user=user, status=1)
+        files = FileModel.objects.filter(user=user)
+        context = super(ShowImageAllView, self).get_context_data(**kwargs)
+        context['username'] = username
+        context['images'] = images
+        context['files'] = files
+        return context
 
 
 @login_required
@@ -162,13 +275,26 @@ def upload_file(request):
     return render(request, 'upload_app/upload_file.html', {'form': form, 'username': username})
 
 
-@login_required
-def show_file(request):
-    user = request.user
-    username = user.username
-    files = FileModel.objects.filter(user=user)
-    content = {'files': files, 'username': username}
-    return render(request, 'upload_app/show_file.html', content)
+# @login_required
+# def show_file(request):
+#     user = request.user
+#     username = user.username
+#     files = FileModel.objects.filter(user=user)
+#     content = {'files': files, 'username': username}
+#     return render(request, 'upload_app/show_file.html', content)
+
+
+class ShowFileView(TemplateView):
+    template_name = 'upload_app/show_file.html'
+
+    def get_context_data(self, **kwargs):
+        user = self.request.user
+        username = user.username
+        files = FileModel.objects.filter(user=user)
+        context = super(ShowFileView, self).get_context_data(**kwargs)
+        context['username'] = username
+        context['files'] = files
+        return context
 
 
 @login_required
